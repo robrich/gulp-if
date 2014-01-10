3edaaef450e9dce7c5abedae08012ca2a8a8c74e
Alberto Leal <mail4alberto@gmail.com>
2014-01-10 17:17:28 -0500

    Be able to use a boolean function

--- index.js
+++ index.js
@@ -8,6 +8,31 @@ module.exports = function (doit, child) {
 	if (!child) {
 		throw new Error('gulp-if: child action is required');
 	}
+	
+	// if doit is a boolean function
+	if(typeof doit === 'function') {
+		var _write = function(file) {
+		
+		    if(!!doit.call(null, file)) {
+		            
+		            var get_this = this;
+		
+		            var detour = through()
+		                    .pipe(child)
+		                    .through(function(data) {get_this.emit('data', data);});
+		
+		            return detour.write(file);
+		        
+		    } else {
+		        // pass along
+		        return this.emit('data' file);
+		    }
+		
+		}
+		
+		return through(_write);
+	}
+	
 	if (doit) {
 		return child;
 	} else {
