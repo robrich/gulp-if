2bf2c830d71616de6cbdd3e8075dd6cff41f16ad
Alberto Leal <mail4alberto@gmail.com>
2014-01-10 18:39:42 -0500

    Add tests and refactor.

--- index.js
+++ index.js
@@ -5,40 +5,44 @@
 var through = require('through');
 
 module.exports = function (doit, child) {
-	if (!child) {
-		throw new Error('gulp-if: child action is required');
-	}
-	
-	// if doit is a boolean function
-	if(typeof doit === 'function') {
-		var _write = function(file) {
-		
-		    if(!!doit.call(null, file)) {
-		            
-		            var get_this = this;
-		
-		            var detour = through()
-		                    .pipe(child)
-		                    .through(function(data) {get_this.emit('data', data);});
-		
-		            return detour.write(file);
-		        
-		    } else {
-		        // pass along
-		        return this.emit('data' file);
-		    }
-		
-		}
-		
-		return through(_write);
-	}
-	
-	if (doit) {
-		return child;
-	} else {
-		// noop
-		return through(function(data){
-			this.emit('data', data);
-		});
-	}
+    if (!child) {
+        throw new Error('gulp-if: child action is required');
+    }
+
+    // if doit is a boolean function
+    if(typeof doit === 'function') {
+
+        var _write = function(file) {
+
+            if(!!doit.call(null, file)) {
+
+                this.pause();
+
+                var detour = through();
+
+                detour.pipe(child)
+                .pipe(through(this.push));
+
+                detour.once('end', this.resume);
+
+                detour.write(file), detour.end();
+
+            } else {
+                // pass along
+                this.emit('data', file);
+            }
+
+        }
+
+        return through(_write);
+    }
+
+    if (doit) {
+        return child;
+    } else {
+        // noop
+        return through(function(data){
+            this.emit('data', data);
+        });
+    }
 };
