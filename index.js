d1c8b60798edddd513e0522c216b435a78662948
Rob Richardson <robrich@robrich.org>
2014-05-23 21:04:44 -0700

    If the condition is a boolean, no need to check the condition for every file, added benefit is it won't ever initialize the non-matching stream

--- index.js
+++ index.js
@@ -2,10 +2,17 @@
 
 var match = require('gulp-match');
 var ternaryStream = require('ternary-stream');
+var through2 = require('through2');
 
 module.exports = function (condition, trueChild, falseChild) {
 	if (!trueChild) {
 		throw new Error('gulp-if: child action is required');
+	}
+
+	if (typeof condition === 'boolean') {
+		// no need to evaluate the condition for each file
+		// other benefit is it never loads the other stream
+		return condition ? trueChild : (falseChild || through2.obj());
 	}
 
 	function classifier (file) {
--- test/boolean.js
+++ test/boolean.js
@@ -75,5 +75,43 @@ describe('gulp-if', function() {
 			s.write(fakeFile);
 			s.end();
 		});
+
+		it('should call the false function when passed truthy', function(done) {
+			// Arrange
+			var condition = false;
+			var called = 0;
+			var fakeFile = {
+				path: tempFile,
+				contents: new Buffer(tempFileContent)
+			};
+
+			var s = gulpif(condition, through.obj(function (file, enc, cb) {
+				// Test that file got passed through
+				(file === fakeFile).should.equal(true);
+
+				called+=10;
+				this.push(file);
+				cb();
+			}), through.obj(function (file, enc, cb) {
+				// Test that file got passed through
+				(file === fakeFile).should.equal(true);
+
+				called++;
+				this.push(file);
+				cb();
+			}));
+
+			// Assert
+			s.once('finish', function(){
+
+				// Test that command executed
+				called.should.equal(1);
+				done();
+			});
+
+			// Act
+			s.write(fakeFile);
+			s.end();
+		});
 	});
 });
