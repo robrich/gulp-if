0d8e64f703f58c04efa90f38e1e40ed00761550a
Fabio Spampinato <spampinabio@gmail.com>
2018-02-23 19:34:03 +0100

    Support for lazy evaluation (Fixes #75)

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ var match = require('gulp-match');
 var ternaryStream = require('ternary-stream');
 var through2 = require('through2');
 
+function lazyTransform (lazyFn) {
+	var fnCached;
+	var transform = function () {
+		var fn = fnCached || lazyFn ();
+		fn._transform.apply ( fn, arguments );
+	};
+	return through2.obj(transform);
+}
+
+function lazify (lazyFn) {
+	return ( !lazyFn || lazyFn._transform ) ? lazyFn : lazyTransform (lazyFn);
+}
+
 module.exports = function (condition, trueChild, falseChild, minimatchOptions) {
 	if (!trueChild) {
 		throw new Error('gulp-if: child action is required');
@@ -12,12 +25,12 @@ module.exports = function (condition, trueChild, falseChild, minimatchOptions) {
 	if (typeof condition === 'boolean') {
 		// no need to evaluate the condition for each file
 		// other benefit is it never loads the other stream
-		return condition ? trueChild : (falseChild || through2.obj());
+		return condition ? lazify(trueChild) : (falseChild ? lazify(falseChild) : through2.obj());
 	}
 
 	function classifier (file) {
 		return !!match(file, condition, minimatchOptions);
 	}
 
-	return ternaryStream(classifier, trueChild, falseChild);
+	return ternaryStream(classifier, lazify(trueChild), lazify(falseChild));
 };
